b1ae5e142cb88fee085723192d37692a093151cf
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-07-08 21:32:31 -0400

    first commit

---
+++ Project/airline_analysis.sql
@@ -0,0 +1,4 @@
+-- Analyze airline performance data to understand flight delays, cancellations, and factors affecting on-time performance.
+-- You will extract, transform, and load (ETL) the data, perform SQL queries for data analysis, and create an interactive dashboard in Power BI to present your findings.
+
+
